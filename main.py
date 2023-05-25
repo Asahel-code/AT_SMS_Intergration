a30a8fa52bc418cfbe23e00c5528079ff3d44fe1
Asahel Enock Kigen <kirwaasahel@gmail.com>
2023-05-25 16:15:10 +0300

    [INIT]: Sms project Intialization

---
+++ main.py
@@ -0,0 +1,23 @@
+import os
+import africastalking
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Initialize SDK
+username = os.environ.get('AT_USERNAME')  
+api_key = os.environ.get('AT_APIKEY')      
+africastalking.initialize(username, api_key)
+
+
+# Initialize a service e.g. SMS
+sms = africastalking.SMS
+
+
+#Use it asynchronously
+def on_finish(error, response):
+    if error is not None:
+        raise error
+    print(response)
+
+sms.send("Hello Message!", ["+254711959117"], callback=on_finish)
